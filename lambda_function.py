efd5165dd5da8e386f76217d1126e7231cd2339b
Garima-tl <130911547+Garima-tl@users.noreply.github.com>
2024-06-13 15:16:28 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def  lambda_handler(event,context):
-    d= {'ceil':[1,2],'ceil2': [3,4]}
+    d= {'ceil':[5,6],'ceil2': [3,4]}
     df = pd.DataFrame(data=d)
     print(df)
     print('Done x1.1')
